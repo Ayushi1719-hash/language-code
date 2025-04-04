fee2168134e5a8c1a908ed5ebaf1596bc73c8a0b
Ayushi Sinha <Ayushi.Sinha@bounteous.com>
2025-04-04 12:39:38 +0530

    Update program_3.cpp

--- program_3.cpp
+++ program_3.cpp
@@ -1,40 +1,46 @@
-# include <iostream>
+#include <iostream>
 using namespace std;
 
 int main() {
 
-  char op;
-  float num1, num2;
+    char op;
+    float operand1, operand2;
 
-  cout << "Enter operator: +, -, *, /: ";
-  cin >> op;
+    cout << "Enter operator (+, -, *, /, %): ";
+    cin >> op;
 
-  cout << "Enter two operands: ";
-  cin >> num1 >> num2;
+    cout << "Enter two operands: ";
+    cin >> operand1 >> operand2;
 
-  switch(op) {
+    switch(op) {
 
-    case '+':
-      cout << num1 << " + " << num2 << " = " << num1 + num2;
-      break;
+        case '+':
+            cout << operand1 << " + " << operand2 << " = " << operand1 + operand2;
+            break;
 
-    case '-':
-      cout << num1 << " - " << num2 << " = " << num1 - num2;
-      break;
+        case '-':
+            cout << operand1 << " - " << operand2 << " = " << operand1 - operand2;
+            break;
 
-    case '*':
-      cout << num1 << " * " << num2 << " = " << num1 * num2;
-      break;
+        case '*':
+            cout << operand1 << " * " << operand2 << " = " << operand1 * operand2;
+            break;
 
-    case '/':
-      cout << num1 << " / " << num2 << " = " << num1 / num2;
-      break;
+        case '/':
+            if (operand2 != 0)
+                cout << operand1 << " / " << operand2 << " = " << operand1 / operand2;
+            else
+                cout << "Error: Division by zero!";
+            break;
 
-    default:
-      // If the operator is other than +, -, * or /, error message is shown
-      cout << "Error! operator is not correct";
-      break;
-  }
+        case '%':
+            cout << "Modulo = " << static_cast<int>(operand1) % static_cast<int>(operand2);
+            break;
 
-  return 0;
+        default:
+            cout << "Error! Operator is not correct";
+            break;
+    }
+
+    return 0;
 }
