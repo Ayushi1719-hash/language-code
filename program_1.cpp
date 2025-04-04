5eb481ea8da238a3254c21c3bc9a7fb4e2434e63
Ayushi Sinha <Ayushi.Sinha@bounteous.com>
2025-04-04 12:38:57 +0530

    Update program_1.cpp

--- program_1.cpp
+++ program_1.cpp
@@ -3,51 +3,53 @@ using namespace std;
 
 class Complex
 {
-    private:
-      float real;
-      float imag;
-    public:
-       Complex(): real(0), imag(0){ }
-       void input()
-       {
-           cout << "Enter real and imaginary parts respectively: ";
-           cin >> real;
-           cin >> imag;
-       }
+private:
+    float real;
+    float imag;
 
-       // Operator overloading
-       Complex operator - (Complex c2)
-       {
-           Complex temp;
-           temp.real = real - c2.real;
-           temp.imag = imag - c2.imag;
+public:
+    Complex() : real(0), imag(0) { }
+    Complex(float r, float i) : real(r), imag(i) { }
 
-           return temp;
-       }
+    void input()
+    {
+        cout << "Enter real and imaginary parts respectively: ";
+        cin >> real >> imag;
+    }
 
-       void output()
-       {
-           if(imag < 0)
-               cout << "Output Complex number: "<< real << imag << "i";
-           else
-               cout << "Output Complex number: " << real << "+" << imag << "i";
-       }
+    // Operator overloading
+    Complex operator - (Complex c2)
+    {
+        Complex temp;
+        cout << "Subtracting: (" << real << "+" << imag << "i) - ("
+             << c2.real << "+" << c2.imag << "i)\n";
+        temp.real = real - c2.real;
+        temp.imag = imag - c2.imag;
+
+        return temp;
+    }
+
+    void display()
+    {
+        if (imag < 0)
+            cout << "Result: " << real << imag << "i";
+        else
+            cout << "Result: " << real << "+" << imag << "i";
+    }
 };
 
 int main()
 {
     Complex c1, c2, result;
 
-    cout<<"Enter first complex number:\n";
+    cout << "Enter first complex number:\n";
     c1.input();
 
-    cout<<"Enter second complex number:\n";
+    cout << "Enter second complex number:\n";
     c2.input();
 
-    // In case of operator overloading of binary operators in C++ programming, 
-    // the object on right hand side of operator is always assumed as argument by compiler.
     result = c1 - c2;
-    result.output();
+    result.display();
 
     return 0;
 }
